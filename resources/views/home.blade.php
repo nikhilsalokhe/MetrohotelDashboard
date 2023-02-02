fbb51ac58694ec290774d4ec0da22ec5993c215f
[AnkitaLpatil] <[ankitapatil9759@gmail.com]>
2023-02-02 17:29:46 +0530

    change in sidebar and catr create

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,66 +3,83 @@
 @section('content')
 <div class="container">
 <div class="row">
-    <div class="col-lg-3 col-6">
+    <div class="col-lg-2 col-4">
       <!-- small box -->
       <div class="small-box bg-info">
         <div class="inner">
-          <h3>150</h3>
+          <h3>count</h3>
 
-          <p>New Orders</p>
+          <marquee>Total Hotels customers</marquee>
         </div>
-        <div class="icon">
-          <i class="ion ion-bag"></i>
-        </div>
+        
         <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
       </div>
     </div>
     <!-- ./col -->
-    <div class="col-lg-3 col-6">
+    <div class="col-lg-2 col-4">
       <!-- small box -->
       <div class="small-box bg-success">
         <div class="inner">
-          <h3>53<sup style="font-size: 20px">%</sup></h3>
+          <h3>count<sup style="font-size: 20px"></sup></h3>
 
-          <p>Bounce Rate</p>
+          <marquee>Today's Visited Customers</marquee>
         </div>
-        <div class="icon">
-          <i class="ion ion-stats-bars"></i>
-        </div>
+       
         <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
       </div>
     </div>
     <!-- ./col -->
-    <div class="col-lg-3 col-6">
+    <div class="col-lg-2 col-4">
       <!-- small box -->
       <div class="small-box bg-warning">
         <div class="inner">
-          <h3>44</h3>
+          <h3>count</h3>
 
-          <p>User Registrations</p>
+          <marquee>This Week's Customers</marquee>
         </div>
-        <div class="icon">
-          <i class="ion ion-person-add"></i>
-        </div>
+        
         <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
       </div>
     </div>
     <!-- ./col -->
-    <div class="col-lg-3 col-6">
+    <div class="col-lg-2 col-4">
       <!-- small box -->
       <div class="small-box bg-danger">
         <div class="inner">
-          <h3>65</h3>
+          <h3>count</h3>
 
-          <p>Unique Visitors</p>
+          <marquee>This month's Customers</marquee>
         </div>
-        <div class="icon">
-          <i class="ion ion-pie-graph"></i>
-        </div>
+        
         <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
       </div>
     </div>
     <!-- ./col -->
+    <div class="col-lg-2 col-4">
+      <!-- small box -->
+      <div class="small-box bg-primary text-white">
+        <div class="inner">
+          <h3>count</h3>
+
+          <marquee>Monthly Avrage Bookings </marquee>
+        </div>
+        
+        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+      </div>
+    </div>
+    <div class="col-lg-2 col-4">
+      <!-- small box -->
+      <div class="small-box bg-secondary text-white">
+        <div class="inner">
+          <h3>count</h3>
+
+          <marquee>Monthly Avrage RoomRent</marquee>
+        </div>
+        
+        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+      </div>
+    </div>
   </div>
 </div>
+
 @endsection
